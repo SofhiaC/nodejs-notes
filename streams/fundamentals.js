1128139203200e5c93ff378ef41c6050489c6efc
Sofhia Chromiec <sofhia.chromiec@gmail.com>
2025-10-25 22:11:21 -0300

    streams no node

---
+++ streams/fundamentals.js
@@ -0,0 +1,27 @@
+//não é possível ler formatos primitivos na stream, então deve se trabalhar com o formato buffer, e o buffer não aceita números
+
+import { Readable } from 'node:stream'
+import { buffer } from 'node:stream/consumers'
+
+class OneToHundredStream extends Readable { 
+    index = 1
+    _read() { //toda stream tem obrigatóriamente o método read, stream de leitura
+        const i = this.index++
+
+        setTimeout( () => {
+            if (i > 100){
+                this.push(null) //push é o método pra stream fornecer as informações pra quem tá consumindo ela, null diz que não há nada pra ser enviado dessa stream
+            } else {
+                const buf = Buffer.from(String(i))
+                this.push(buf)
+            }
+        }, 1000) 
+    }
+}
+
+new OneToHundredStream()
+    .pipe(process.stdout)
+
+//é possível trabalhar com arquivos grandes, ler eles e mostrar como simualdo mas isso poderiam ser arquivos reais
+//OU SEJA, trabalhar com dados mesmo que eles estejam incompletos 
+
